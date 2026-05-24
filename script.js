0ad17fd4311596bd24a0152c2708fa9e5788de10
jefferson alves <j22alves.s@gmail.com>
2026-05-23 22:51:33 -0300

    quarto commit do js

--- script.js
+++ script.js
@@ -33,10 +33,10 @@ function receberQuatidadePessoas(evento){
 
 const botoesGorjeta = document.querySelectorAll(".gorjeta input[Type='button']")
 botoesGorjeta.forEach(botao =>{
-    botao.addEventListener("click",receberPorcentagemBotao )
+    botao.addEventListener("click",receberPorcentagem )
 })
 
-function receberPorcentagemBotao(evento){
+function receberPorcentagem(evento){
     botoesGorjeta.forEach(botao =>{
         botao.classList.remove("botao-ativo")
 
@@ -45,5 +45,15 @@ function receberPorcentagemBotao(evento){
            botao.classList.add("botao-ativo") 
         }
     })
-   porcentagem = parseFloat(evento.target.value) /100
+
+    if(evento.target.value !== ""){
+porcentagem = parseFloat(evento.target.value) /100
+    }else{
+        porcentagem = 0
+    }
+   
+   console.log("porcentagem")
 }
+
+const gorjetaInput = document.querySelector("#outra")
+gorjetaInput.addEventListener("input",receberPorcentagem)
